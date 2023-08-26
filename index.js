4214ca5ef82f305c1783be3941467fc2246f0e21
imoteriannah <imoter.iannah@student.moringaschool.com>
2023-08-26 10:23:25 +0100

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = "Paul";
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
